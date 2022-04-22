b1f2f549f7e1bef52d6e1704254d9d75066f8b4b
Blissling <unconfigured@null.spigotmc.org>
2022-04-22 12:05:49 +0200

    Updated Calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,5 +1,5 @@
 import re
-import pandas
+# import pandas
 
 class User:
     def __init__(self, name, score):
@@ -8,11 +8,9 @@ class User:
 
 lines = []
 
-print("Reading file...")
-with open("C:\\Users\\chris\\Documents\\Github\\UNTITLED\\Calculator\\testmessages.txt", "r", encoding="utf8") as file:
+with open("testmessages.txt", "r", encoding="utf8") as file:
     for i in file:
         lines.append(i.rstrip("\n"))
-print("Done!")
 
 # for i in lines:       # line by line
 #     print(i)          # print the txt file
